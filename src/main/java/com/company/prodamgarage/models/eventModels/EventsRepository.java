90195d3fe578916a17d4bdea765d7899dd368e5e
CACTUS0504 <mat.zaharoff@yandex.ru>
2022-09-24 23:34:00 +0300

    mes

--- src/main/java/com/company/prodamgarage/models/eventModels/EventsRepository.java
+++ src/main/java/com/company/prodamgarage/models/eventModels/EventsRepository.java
@@ -10,17 +10,23 @@ public class EventsRepository {
         return goodEventList;
     }
 
-    public void setGoodEventList(List<Event> goodEventList) { this.goodEventList = goodEventList; }
+    public void setGoodEventList(List<Event> goodEventList) {
+        this.goodEventList = goodEventList;
+    }
 
     public List<Event> getBadEventList() {
         return badEventList;
     }
 
-    public void setBadEventList(List<Event> badEventList) { this.badEventList = badEventList; }
+    public void setBadEventList(List<Event> badEventList) {
+        this.badEventList = badEventList;
+    }
 
     public Event getRandomGoodEvent() {
         return (goodEventList.get((int) (Math.random() * goodEventList.size())));
     }
 
-    public Event getRandomBadEvent() { return (badEventList.get((int) (Math.random() * badEventList.size()))); }
+    public Event getRandomBadEvent() {
+        return (badEventList.get((int) (Math.random() * badEventList.size())));
+    }
 }
